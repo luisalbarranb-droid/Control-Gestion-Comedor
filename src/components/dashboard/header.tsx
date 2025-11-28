1a69ffb605f0d4da2096ea1f2209ba349dcc900b
arvecladu <arvecladu@gmail.com>
2025-11-28 22:38:23 +0000

    ES EL MENÚ LATERAL QUE TAPA DEBE APARECER Y DESAPARECER CUANDODACORRESPO

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -3,7 +3,7 @@ import { UserNav } from '@/components/dashboard/user-nav';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur md:px-6">
+    <header className="sticky top-0 z-40 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur md:px-6">
       <SidebarTrigger className="md:hidden" />
       <div className="w-full flex-1">
         {/* Can add breadcrumbs or search here */}
