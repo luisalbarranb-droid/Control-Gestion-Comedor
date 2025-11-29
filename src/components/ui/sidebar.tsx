c15e55659f4bf48cc67cfe8ba935590a9e6cc1c6
arvecladu <arvecladu@gmail.com>
2025-11-29 23:48:13 +0000

    EL MENU LATERAL NO AUN NO APARCE  REVISEMOS CON DETENIMIENTO

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -168,7 +168,7 @@ const Sidebar = React.forwardRef<
     {
       side = "left",
       variant = "sidebar",
-      collapsible = "offcanvas",
+      collapsible = "icon",
       className,
       children,
       ...props
@@ -177,22 +177,7 @@ const Sidebar = React.forwardRef<
   ) => {
     const { isMobile, state, openMobile, setOpenMobile } = useSidebar()
 
-    if (collapsible === "none") {
-      return (
-        <div
-          className={cn(
-            "flex h-full w-[--sidebar-width] flex-col bg-sidebar text-sidebar-foreground",
-            className
-          )}
-          ref={ref}
-          {...props}
-        >
-          {children}
-        </div>
-      )
-    }
-    
-    if (isMobile || collapsible === "offcanvas") {
+    if (isMobile) {
       return (
         <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
           <SheetContent
