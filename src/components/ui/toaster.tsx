7435d06c3373d760704a8381df0b4c93b6743889
arvecladu <arvecladu@gmail.com>
2025-12-09 00:21:57 +0000

    "use client"
    import {
      Toast,
      ToastClose,
      ToastDescription,
      Toas

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
+  useToast,
 } from "@/components/ui/toast"
-import { useToast } from "@/hooks/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
